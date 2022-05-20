f6a9624dba3408c10a56e2ff4c6415b03da4e68e
Auto Logon <autologon@CSED.LAB>
2022-05-20 20:36:58 -0300

    agora com BD

---
+++ apiCARROS/script_bd.sql
@@ -0,0 +1,16 @@
+create database dbApiCarros;
+
+use dbApiCarros;
+
+create table carros
+(
+codigo int primary key auto_increment,
+modelo varchar(30),
+placa varchar(7)
+);
+
+insert into carros (modelo, placa) value ('Toyota Corolla', 'GGG3535');
+insert into carros (modelo, placa) value ('Honda Civic', 'ELV1590');
+insert into carros (modelo, placa) value ('Honda Fit', 'BKI1533');
+
+select * from carros;
