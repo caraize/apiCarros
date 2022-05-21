3cb9b1fa8e324ef6279ac4403489fdde44e5a9a3
Auto Logon <autologon@CSED.LAB>
2022-05-20 21:06:12 -0300

    Final consulta de todos os carros

--- apiCARROS/src/controllers/CarroController.js
+++ apiCARROS/src/controllers/CarroController.js
@@ -1,1 +1,18 @@
 const CarroServices = require('../services/CarroService');
+
+module.exports = {
+    buscarTodos: async (req,res) => {
+        let json = {error:'', result:[]};
+
+        let carros = await CarroServices.buscarTodos();
+
+        for(let i in carros){
+            json .result.push({
+                codigo: carros[i].codigo,
+                descrição: carros[i].modelo
+            });
+        }
+
+        res.json(json);
+    }
+}
