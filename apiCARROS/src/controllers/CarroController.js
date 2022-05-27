3024a07629aa82e90e9475a3b7a5c3d299a20c6c
Auto Logon <autologon@CSED.LAB>
2022-05-27 20:20:44 -0300

    criação da inserção

--- apiCARROS/src/controllers/CarroController.js
+++ apiCARROS/src/controllers/CarroController.js
@@ -14,5 +14,40 @@ module.exports = {
         }
 
         res.json(json);
+    },
+
+    buscarUm: async (req,res) => {
+        let json = {error:'', result:[]};
+
+        let codigo = req.params.codigo; //pegar o parametro e armazenar na variavel codigo
+        let carro = await CarroServices.buscarUm(codigo);
+
+        if(carro){
+            json.result=carro; //verificar se tem carro, se tiver joga no json 
+        }
+
+        res.json(json);
+    },
+
+    
+    inserir: async (req,res) => {
+        let json = {error:'', result:[]};
+
+        let modelo = req.body.modelo
+        let placa = req.body.placa
+     
+        if(modelo && placa){
+            let carroCodigo = await CarroServices.inserir(modelo, placa);
+            json.result={
+                codigo:carroCodigo,
+                modelo,
+                placa
+            }
+        }else {
+            json.error = 'Campos nao encontrados';
+        }
+    
+        res.json(json);
     }
+    
 }
--- apiCARROS/src/services/CarroService.js
+++ apiCARROS/src/services/CarroService.js
@@ -10,4 +10,34 @@ module.exports = {
             });
         });
     }
+},
+module.exports = {
+    buscarUm: (codigo)=>{
+        return new Promise((aceito, rejeitado)=>{
+
+            db.query('SELECT * FROM carros where codigo = ?', [codigo], (error,results)=>{
+                if (error) {rejeitado(error);return;}
+                if(results.length > 0){
+                    aceito(results[0]);
+                }
+                else{
+                    aceito(false);
+                    }
+            });
+        });
+    },
+    
+
+   
+    inserir: (modelo, placa)=>{
+        return new Promise((aceito, rejeitado)=>{
+
+            db.query('insert into carros (modelo, placa) values (?,?)',
+                [modelo, placa], 
+                (error,results)=>{
+                    if(error) {rejeitado(error);return;}
+                    aceito(results.insertCodigo);                
+            });
+        });
+    }
 }
